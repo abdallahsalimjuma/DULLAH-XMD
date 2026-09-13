ea8704845b289c4606b7d0a94b5409cf37afc666
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2026-09-13 21:24:49 +0300

    Rename script from botimage.js to imagee.js

--- dullah/imagee.js
+++ dullah/imagee.js
@@ -2,7 +2,7 @@ use strict';
 
 const axios = require('axios');
 
-const scriptName = 'botimage.js';
+const scriptName = 'imagee.js';
 const scriptUrl = `https://dullah-xmd-commands-seven.vercel.app/${scriptName}`;
 
 async function loadScript() {
